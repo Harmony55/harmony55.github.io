5915797507e2edc852d7e763bd34083368ababf0
Harmony55 <103098112+Harmony55@users.noreply.github.com>
2022-04-06 09:07:02 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,182 @@
+questions = [
+    {
+        "question": "Il est très important d'archiver le plus possible.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Buff bananakat",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Twitch et ses conséquences on été terrible pour l'humanité.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "J'ai déjà commis au moins un (1) crime de guerre en Yougoslavie en 1999.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Je vie dans un paradis fiscale.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Mettre des archives dans un bus est stupide.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Les g@mers devrais acheter un Twitch Prime.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Cludine est très équilibrer.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "J'ai achter une femme pour 10k chocolat.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Les présidents de BBCP on toujour raison.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": ":mdr:",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Les nouveaux design des derrière des cartes est mieu que l'ancien.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "J'utilise souvent les emojis de BBCP.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Twitch devrais fermer pour laisser place à youtube.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 20,
+            "scty": 0
+        }
+    },
+    {
+        "question": "lé fan de twitch nont pas de vie evidement",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "moi ce qui me tue c'est que des gens préfèrent YouT*be à Twitch...",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -20,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Il faut changer les présidents de BBCP",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Le grand rework était nécessaire",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Nerf Sharko",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Pour accéder à certaines archvies, il faut montrer en être digne via un pass.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    }
+];
